9498d27aa691d3159727eab322eb1d084b7ba423
illegal3alien <nijave@gmail.com>
2010-10-03 14:06:37 -0400

    Small tweaks

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ $i = 0;
 				<!-- I added a simple PHP number increment which is used to determine the correct offset for the triangle arrow
 					The arrow is always lined up (few px off) no matter which modules are active -->
 				<li><a href="javascript:switchto('about', <? echo $i++; ?>);" id="nav_about">about</a></li>
-				<? if ($images) { ?><li><a href="javascript:switchto('photos', <? echo $i++; ?>);" id="nav_photos">photos</a></li><? } ?>
+				<? if ($images) { ?><li><a href="javascript:switchto('photos', <? echo $i++; ?>);" onclick="" id="nav_photos">photos</a></li><? } ?>
 				<? if ($videos) { ?><li><a href="javascript:switchto('videos', <? echo $i++; ?>);" id="nav_videos">videos</a></li><? } ?>
 				<? if ($twitter) { ?><li><a href="javascript:switchto('twitter', <? echo $i++; ?>);" id="nav_twitter">twitter</a></li><? } ?>
 			</ol>
